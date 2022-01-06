01306b36c1394e2f32154e6d9bec33b3d80e70a9
smrutikapatil <smruti.patil17@gmail.com>
2022-01-06 19:30:12 +0530

    added- reaches for a month

--- src/com/EmpwageComputation/EmployeeWage.java
+++ src/com/EmpwageComputation/EmployeeWage.java
@@ -5,29 +5,32 @@ public class EmployeeWage {
 	public static final int is_parttime_Present = 2;
 	public static final int wage_pr_hr=20;
 	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HOURS_FOR_MONTH = 100;
 	
-	    public static void main(String[] args) {
+	public static void main(String[] args) {
 		getMonthlyEmpWage();
-		}
+    }
 	static void getMonthlyEmpWage() {
-		int working_hour = 0;
-		int daily_wage = 0;
-		int totalEmpwage = 0;
-		for(int day=0; day < NUM_OF_WORKING_DAYS; day++){
-			int emp_check=(int)(Math.floor(Math.random()* 10) % 3);
-			switch (emp_check) {
-			case is_parttime_Present:
-				working_hour=4;
+		int empHrs= 0, totalEmpHrs= 0, totalWorkingDays=0;
+		//Computation
+		while(totalEmpHrs <= MAX_HOURS_FOR_MONTH &&
+			   totalWorkingDays < NUM_OF_WORKING_DAYS) { 
+			   totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10 ) %3;
+			switch(empCheck) {
+			case is_parttime_Present :
+				empHrs=4;
 				break;
 			case is_fulltime_Present:
-				working_hour=8;
+				empHrs=8;
 				break;
 			default:
-				working_hour=0;
+			empHrs=0;
 		}
-		daily_wage = working_hour * wage_pr_hr;
-		totalEmpwage = totalEmpwage + daily_wage;
-		System.out.println("Daily_wage: " + daily_wage);
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " +empHrs);
 		}
-  }
+	int totalEmpWage = totalEmpHrs * wage_pr_hr;
+	System.out.println("Total Emp Wage: " + totalEmpWage);
+ }
 }
