a2de18acae29839d267d6c45c51bb06be65e3319
smrutikapatil <smruti.patil17@gmail.com>
2022-01-06 19:13:32 +0530

    Added- calculated part time employee wage

--- src/com/EmpwageComputation/EmployeeWage.java
+++ src/com/EmpwageComputation/EmployeeWage.java
@@ -1,38 +1,32 @@
 package com.EmpwageComputation;
 
 public class EmployeeWage {
-	public static boolean attendance() {
-		int is_Present = 1;
-		int emp_check=(int)(Math.floor(Math.random()* 10) % 2);
-		if (emp_check==is_Present) 
-			return true;  	
-			else 
-			   return false;
-	}
-	public static int dailywage() {
-		boolean result = attendance();
-		EmployeeWage obj1 = new EmployeeWage();
-		obj1.attendance();
-		int  daily_wage;
-		int wage_pr_hr=20;
-	    int working_hour;
-		
-		if (result)
-		{
-			System.out.println("Employee is Present");
-			working_hour=8;
-		} 
-		else
-		{
-			System.out.println("Employee is Absent");
-	        working_hour=0;
-		}
-		daily_wage=working_hour * wage_pr_hr;
-		return daily_wage;
-	}
+	public static final int is_fulltime_Present = 1;
+	public static final int is_parttime_Present = 2;
+	public static final int wage_pr_hr=20;
+
 	public static void main(String[] args) {
-		int result = dailywage();
-	
-	System.out.println("Employee daily wage is " + result);
- }
+		getFullOrPartTimeWage();
+		}
+	static void getFullOrPartTimeWage() {
+	int working_hour = 0;
+	int daily_wage = 0;
+	int emp_check=(int)(Math.floor(Math.random()* 10) % 3);
+	if (emp_check==is_fulltime_Present)
+	{
+		System.out.println("Employee is full time Present");
+		working_hour=8;
+	}
+	else if (emp_check==is_parttime_Present) {
+		System.out.println("Employee is part time present");
+        working_hour=4;
+    }
+	else
+	{
+		System.out.println("Employee is Absent");
+		working_hour=0;
+	}
+	 daily_wage = working_hour * wage_pr_hr;
+	  System.out.println("Employee daily wage is " + daily_wage);  
+	}
 }
