2914f29a8a2ccfb251a6957916d77563883bf2d3
smrutikapatil <smruti.patil17@gmail.com>
2022-01-10 13:36:11 +0530

    Refactor- Ability to Save The Total Wage For Each Company

---
+++ src/com/EmpwageComputation/CompanyEmpWage.java
@@ -0,0 +1,23 @@
+package com.EmpwageComputation;
+
+public class CompanyEmpWage {
+	public final String Company;
+	public final int EmpRatePerHour;
+	public final int NumOfWorkingDays ;
+	public final int MaxHoursPerMonth;
+	private int TotalEmpWage;
+	 
+	public CompanyEmpWage(String Company , int EmpRatePerHour, int NumOfWorkingDays, int MaxHoursPerMonth){
+		this.Company = Company;
+		this.EmpRatePerHour = EmpRatePerHour;
+		this.NumOfWorkingDays = NumOfWorkingDays;
+		this.MaxHoursPerMonth = MaxHoursPerMonth;
+	}
+	public void setTotalEmpWage(int TotalEmpWage) {
+		this.TotalEmpWage = TotalEmpWage;
+	}
+	//@override
+		public String toString() {
+			return "Total Emp Wage for Company: " +Company+ " is: " +TotalEmpWage;
+   }
+}
---
+++ src/com/EmpwageComputation/EmpWagebBuilderObject.java
@@ -0,0 +1,56 @@
+package com.EmpwageComputation;
+
+public class EmpWagebBuilderObject{
+	public static final int Is_Fulltime_Present = 1;
+	public static final int Is_Parttime_Present = 2;
+	 
+	public final String Company;
+	public final int EmpRatePerHour;
+	public final int NumOfWorkingDays ;
+	public final int MaxHoursPerMonth;
+	private int TotalEmpWage;
+	 
+	public EmpWagebBuilderObject(String Company , int EmpRatePerHour, int NumOfWorkingDays, int MaxHoursPerMonth){
+		this.Company = Company;
+		this.EmpRatePerHour = EmpRatePerHour;
+		this.NumOfWorkingDays = NumOfWorkingDays;
+		this.MaxHoursPerMonth = MaxHoursPerMonth;
+	}
+	public void ComputeEmpWage() {
+		//variables
+		int EmpHrs= 0, TotalEmpHrs= 0, TotalWorkingDays=0;
+		//Computation
+		while(TotalEmpHrs <= MaxHoursPerMonth  && TotalWorkingDays < NumOfWorkingDays) { 
+			   TotalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10 ) %3;
+			switch(empCheck) {
+			case Is_Parttime_Present:
+				EmpHrs=4;
+				break;
+			case Is_Fulltime_Present:
+				EmpHrs=8;
+				break;
+			default:
+			EmpHrs=0;
+		}
+		TotalEmpHrs += EmpHrs;
+		System.out.println("Day#: " + TotalWorkingDays + " Emp Hr: " +EmpHrs);
+		}
+	      TotalEmpWage = TotalEmpHrs * EmpRatePerHour;
+       }
+	//@override
+	public String toString() {
+		return "Total Emp Wage for Company: " +Company+ " is: " +TotalEmpWage;
+	}
+	public static void main(String[] args) {
+		EmpWagebBuilderObject dmart = new EmpWagebBuilderObject("Dmart", 20, 2, 10);
+		EmpWagebBuilderObject reliance = new EmpWagebBuilderObject("Reliance", 10, 4, 20);
+		EmpWagebBuilderObject vmart = new EmpWagebBuilderObject("Vmart", 30, 3, 30);
+		dmart.ComputeEmpWage();
+		System.out.println(dmart);
+		reliance.ComputeEmpWage();
+		System.out.println(reliance);
+		vmart.ComputeEmpWage();
+		System.out.println(vmart);
+	}
+}
